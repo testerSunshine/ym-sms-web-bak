328fd86efcd91a67aa03ec4f54e2cbf11b7c1a6a
jimwen <931128603@qq.com>
2022-08-29 21:44:39 +0800

    链接转化

---
+++ src/api/system/SysSource.js
@@ -0,0 +1,3 @@
+import {GetApi, PostApi} from "@/api/request"
+
+export const report = new PostApi(`/source/report`)
--- src/view/_app/login/RegisterForm.vue
+++ src/view/_app/login/RegisterForm.vue
@@ -1,55 +1,55 @@
 <template>
-    <el-form ref="form" :model="form" :rules="rules" label-position="left">
-        <el-form-item prop="username">
+  <el-form ref="form" :model="form" :rules="rules" label-position="left">
+    <el-form-item prop="username">
             <span class="svg-container">
                 <v-icon icon="svg-user"/>
             </span>
-            <el-input ref="username" v-model="form.username" :maxlength="20" placeholder="请输入登录名称"/>
-        </el-form-item>
+      <el-input ref="username" v-model="form.username" :maxlength="20" placeholder="请输入您的邮箱" @blur="sendEmail"/>
+    </el-form-item>
 
-        <el-form-item prop="nick">
-            <span class="svg-container">
-                <v-icon icon="svg-user"/>
-            </span>
-            <el-input v-model="form.nick" :maxlength="100" placeholder="请输入昵称"/>
-        </el-form-item>
+<!--    <el-form-item prop="nick">-->
+<!--            <span class="svg-container">-->
+<!--                <v-icon icon="svg-user"/>-->
+<!--            </span>-->
+<!--      <el-input v-model="form.nick" :maxlength="100" placeholder="请输入昵称"/>-->
+<!--    </el-form-item>-->
 
-        <el-form-item prop="pwd">
-            <el-tooltip v-model="capsTooltip" :tabindex="-1" content="大写锁定已打开" manual placement="left">
+    <el-form-item prop="pwd">
+      <el-tooltip v-model="capsTooltip" :tabindex="-1" content="大写锁定已打开" manual placement="left">
                 <span class="svg-container">
                     <v-icon icon="svg-password"/>
                 </span>
-            </el-tooltip>
-            <el-input v-model="form.pwd" placeholder="请输入密码" type="password" :maxlength="20"/>
-        </el-form-item>
+      </el-tooltip>
+      <el-input v-model="form.pwd" placeholder="请输入密码" type="password" :maxlength="20"/>
+    </el-form-item>
 
-        <el-form-item prop="repwd">
+    <el-form-item prop="repwd">
             <span class="svg-container">
                 <v-icon icon="svg-password"/>
             </span>
-            <el-input
-                v-model="form.repwd"
-                placeholder="请确认密码"
-                type="password"
-                :maxlength="20"
-                @keyup.enter.native="register"
-            />
-        </el-form-item>
+      <el-input
+          v-model="form.repwd"
+          placeholder="请确认密码"
+          type="password"
+          :maxlength="20"
+          @keyup.enter.native="register"
+      />
+    </el-form-item>
 
-        <el-button
-            :loading="loading"
-            class="submit-btn"
-            type="primary"
-            @click="register"
-        >
-            {{ loading ? '注 册 中...' : '注 册' }}
-        </el-button>
+    <el-button
+        :loading="loading"
+        class="submit-btn"
+        type="primary"
+        @click="register"
+    >
+      {{ loading ? '注 册 中...' : '注 册' }}
+    </el-button>
 
-        <div class="flex" style="margin-top: 20px">
-            <p/>
-            <el-button type="text" @click="login">已有账户登录</el-button>
-        </div>
-    </el-form>
+    <div class="flex" style="margin-top: 20px">
+      <p/>
+      <el-button type="text" @click="login">已有账户登录</el-button>
+    </div>
+  </el-form>
 </template>
 
 <script>
@@ -59,96 +59,102 @@ import {elSuccess} from "@/util/message"
 import {md5} from "@/util/secret"
 
 export default {
-    name: "RegisterForm",
+  name: "RegisterForm",
 
-    data() {
-        const validateLoginName = debounce((r, v, c) => {
-            checkLoginName
-                .request(this.form.username, this.form.id)
-                .then(({msg}) => msg ? c(msg) : c())
-                .catch(e => c(e))
-        }, 300)
-        const validateNickName = debounce((r, v, c) => {
-            checkNickName
-                .request(this.form.nick, this.form.id)
-                .then(({msg}) => msg ? c(msg) : c())
-                .catch(e => c(e))
-        }, 300)
-        const validateRepwd = (r, v, c) => {
-            return v !== this.form.pwd ? c('两次密码输入不一致') : c()
-        }
-        return {
-            form: {
-                username: '',
-                nick: '',
-                pwd: '',
-                repwd: ''
-            },
-            rules: {
-                username: [
-                    {required: true, message: '请输入登录名称', trigger: 'change'},
-                    {validator: validateLoginName, trigger: 'change'}
-                ],
-                nick: [
-                    {required: true, message: '请输入昵称', trigger: 'change'},
-                    {validator: validateNickName, trigger: 'change'}
-                ],
-                pwd: [
-                    {required: true, message: '请输入密码', trigger: 'change'},
-                    {min: 6, max: 32, message: '请输入6-32位的密码', trigger: 'change'}
-                ],
-                repwd: [
-                    {required: true, message: '请确认密码', trigger: 'change'},
-                    {validator: validateRepwd, trigger: 'change'}
-                ],
-            },
-            capsTooltip: false,
-            loading: false
-        }
-    },
-
-    methods: {
-        register() {
-            if (this.loading) return
-            this.$refs.form.validate(valid => {
-                if (!valid) return
-                this.loading = true
-                register
-                    .request({
-                        username: this.form.username,
-                        nick: this.form.nick,
-                        password: md5(this.form.pwd)
-                    })
-                    .then(() => {
-                        elSuccess('注册成功')
-                        this.$router.push('/login')
-                    })
-                    .catch(() => this.loading = false)
-            })
-        },
-
-        login() {
-            !this.loading && this.$router.push('/login')
-        },
-
-        capsLockTip({keyCode}) {
-            if (keyCode === 20) this.capsTooltip = !this.capsTooltip
-        },
-
-        addEvent() {
-            window.addEventListener('keyup', this.capsLockTip)
-        },
-
-        removeEvent() {
-            window.removeEventListener('keyup', this.addCapsLockEvent)
-        }
-    },
-
-    mounted() {
-        this.addEvent()
-        this.$nextTick(() => this.$refs.username.focus())
-
-        this.$once('hook:beforeDestroy', this.removeEvent)
+  data() {
+    const validateLoginName = debounce((r, v, c) => {
+      checkLoginName
+          .request(this.form.username, this.form.id)
+          .then(({msg}) => msg ? c(msg) : c())
+          .catch(e => c(e))
+    }, 300)
+    const validateNickName = debounce((r, v, c) => {
+      checkNickName
+          .request(this.form.nick, this.form.id)
+          .then(({msg}) => msg ? c(msg) : c())
+          .catch(e => c(e))
+    }, 300)
+    const validateRepwd = (r, v, c) => {
+      return v !== this.form.pwd ? c('两次密码输入不一致') : c()
     }
+    return {
+      form: {
+        username: '',
+        nick: '',
+        pwd: '',
+        repwd: '',
+      },
+      rules: {
+        username: [
+          {required: true, message: '请输入您的邮箱', trigger: 'change'},
+          {validator: validateLoginName, trigger: 'change'}
+        ],
+        pwd: [
+          {required: true, message: '请输入密码', trigger: 'change'},
+          {min: 6, max: 32, message: '请输入6-32位的密码', trigger: 'change'}
+        ],
+        repwd: [
+          {required: true, message: '请确认密码', trigger: 'change'},
+          {validator: validateRepwd, trigger: 'change'}
+        ],
+      },
+      capsTooltip: false,
+      loading: false
+    }
+  },
+
+  methods: {
+    sendEmail: function () {
+      var regEmail = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
+      if (this.form.username != '' && !regEmail.test(this.form.username)) {
+        this.$message({
+          message: '邮箱格式不正确',
+          type: 'error'
+        })
+        this.form.username = ''
+      }
+    },
+    register() {
+      if (this.loading) return
+      this.$refs.form.validate(valid => {
+        if (!valid) return
+        this.loading = true
+        register
+            .request({
+              username: this.form.username,
+              nick: this.form.username,
+              password: md5(this.form.pwd)
+            })
+            .then(() => {
+              elSuccess('注册成功')
+              this.$router.push('/login')
+            })
+            .catch(() => this.loading = false)
+      })
+    },
+
+    login() {
+      !this.loading && this.$router.push('/login')
+    },
+
+    capsLockTip({keyCode}) {
+      if (keyCode === 20) this.capsTooltip = !this.capsTooltip
+    },
+
+    addEvent() {
+      window.addEventListener('keyup', this.capsLockTip)
+    },
+
+    removeEvent() {
+      window.removeEventListener('keyup', this.addCapsLockEvent)
+    }
+  },
+
+  mounted() {
+    this.addEvent()
+    this.$nextTick(() => this.$refs.username.focus())
+
+    this.$once('hook:beforeDestroy', this.removeEvent)
+  }
 }
 </script>
