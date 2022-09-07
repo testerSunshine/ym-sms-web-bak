4e42b917ba4fa98c03e259fb383e741bb8d2d636
zzjj <test@qq>
2022-09-08 00:31:03 +0800

    展示表格变动

--- src/view/admin/index/component/PanelGroup/index.vue
+++ src/view/admin/index/component/PanelGroup/index.vue
@@ -22,11 +22,39 @@
           label="发送数量">
       </el-table-column>
       <el-table-column
+          prop="userMoney"
+          label="价格">
+      </el-table-column>
+      <el-table-column
+          prop="content"
+          label="描述">
+      </el-table-column>
+      <el-table-column
           prop="updateTime"
           label="更新时间">
       </el-table-column>
     </el-table>
-    <p>10条收取验证码的项目名称</p>
+    <p>10条收取验证码的项目+用户</p>
+    <el-table :data="sendSmsFlowLast10List"
+              style="width: 100%">
+      <el-table-column
+          prop="projectId"
+          label="项目id">
+      </el-table-column>
+      <el-table-column
+          prop="projectName"
+          label="项目">
+      </el-table-column>
+      <el-table-column
+          prop="nickName"
+          label="用户">
+      </el-table-column>
+      <el-table-column
+          prop="updateTime"
+          label="更新时间">
+      </el-table-column>
+    </el-table>
+    <p>10条收取验证码的项目详情</p>
     <el-table :data="sendSmsFlowTop10List"
               style="width: 100%">
       <el-table-column
@@ -50,22 +78,53 @@
           label="更新时间">
       </el-table-column>
     </el-table>
-    <p>活跃用户top10账户余额</p>
-    <el-table :data="userWalletEntity"
+
+    <p>活跃用户top10账户余额V2</p>
+    <el-table :data="userWalletEntityV2"
               style="width: 100%">
       <el-table-column
           prop="uid"
           label="uid">
       </el-table-column>
       <el-table-column
+          prop="nickName"
+          label="用户">
+      </el-table-column>
+      <el-table-column
           prop="balances"
           label="余额">
+      </el-table-column>
+      <el-table-column
+          prop="operate"
+          label="变动">
+      </el-table-column>
+      <el-table-column
+          prop="money"
+          label="变动金额">
       </el-table-column>
       <el-table-column
           prop="updateTime"
           label="更新时间">
       </el-table-column>
     </el-table>
+
+
+    <!--    <p>活跃用户top10账户余额</p>-->
+<!--    <el-table :data="userWalletEntity"-->
+<!--              style="width: 100%">-->
+<!--      <el-table-column-->
+<!--          prop="uid"-->
+<!--          label="uid">-->
+<!--      </el-table-column>-->
+<!--      <el-table-column-->
+<!--          prop="balances"-->
+<!--          label="余额">-->
+<!--      </el-table-column>-->
+<!--      <el-table-column-->
+<!--          prop="updateTime"-->
+<!--          label="更新时间">-->
+<!--      </el-table-column>-->
+<!--    </el-table>-->
   </div>
 
 </template>
@@ -137,7 +196,9 @@ export default {
       ],
       sendSmsCodeTop10Data: [],
       sendSmsFlowTop10List: [],
-      userWalletEntit: [],
+      sendSmsFlowLast10List: [],
+      userWalletEntity: [],
+      userWalletEntityV2:[],
     }
   },
 
@@ -153,7 +214,9 @@ export default {
             })
             this.sendSmsCodeTop10Data = data.bizSendSmsTop10EntityList
             this.sendSmsFlowTop10List = data.statisticSendSmsFlowTop10List
-            this.userWalletEntity = data.sysUserWalletEntityList
+            this.sendSmsFlowLast10List = data.bizSendSmsLast10EntityList
+            // this.userWalletEntity = data.sysUserWalletEntityList
+            this.userWalletEntityV2 = data.sysUserWalletActiveLast10EntityList
           })
           .finally(() => this.loading = false)
     },
