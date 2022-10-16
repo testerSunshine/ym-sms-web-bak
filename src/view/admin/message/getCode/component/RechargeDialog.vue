2af7c0eca804f478a5336df230447c05e6667225
jimwen <931128603@qq.com>
2022-10-16 21:25:56 +0800

    充值样式调整

--- src/view/admin/message/getCode/component/RechargeDialog.vue
+++ src/view/admin/message/getCode/component/RechargeDialog.vue
@@ -1,10 +1,11 @@
-<template>
+<template xmlns="http://www.w3.org/1999/html">
   <abstract-dialog :loading="loading" :title="title" :value="value" @close="cancel" @open="open">
     <abstract-form :model="form" :rules="rules">
       <el-form-item label="卡密" prop="cardId">
         <el-input v-model="form.cardId" style="width: 70%"/>
-        <p>说明：从卡密商店购买卡密，获取一串英文+数字序列，然后在这里填入序列，充值对应卡密金币数</p>
-        <el-link type="primary" style="margin-left: 15px" href="https://www.ysfaka.com/links/95FA5D22" target="_blank" :underline="false">卡密商店(如不能访问，请关闭翻墙)</el-link>
+        <el-link type="primary" href="https://www.ysfaka.com/links/95FA5D22" target="_blank" :underline="false">没有卡密？点击购买(如不能访问，请关闭翻墙)</el-link>
+        <br>
+        <span>说明：从卡密商店购买卡密，获取一串英文+数字序列，然后在这里填入序列，充值对应卡密金币数</span>
       </el-form-item>
     </abstract-form>
 
